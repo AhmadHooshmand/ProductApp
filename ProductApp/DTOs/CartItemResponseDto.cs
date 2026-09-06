c5b001ff5229cf88090d7cd161a5c3eec2bc2d49
Ahmad Hooshmand <hdad199271@gmail.com>
2026-09-06 08:21:43 -0700

    CreateCartDTOs

---
+++ ProductApp/DTOs/CartItemResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApp.DTOs
+{
+    public class CartItemResponseDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public Decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ProductApp/DTOs/CartResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductApp.DTOs
+{
+    public class CartResponseDto
+    {
+        public List<CartItemResponseDto> Items { get; set; } = new List<CartItemResponseDto>();
+        public Decimal TotalPrice { get; set; }
+    }
+}
